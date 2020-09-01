822d49ba8eebe50d26c1d69f4905df68d59e3786
Gomah <hi@gomah.fr>
2020-09-01 18:54:23 +1000

    chore(examples): adjust examples

--- examples/next/pages/_app.js
+++ examples/next/pages/_app.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import App from 'next/app';
 
+// Global css
+import '@bulmil/core/dist/css/bulmil.css';
+
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return <Component {...pageProps} />;
   }
-}
-
-if (process.browser) {
-  const { applyPolyfills, defineCustomElements } = require('@bulmil/core/dist/loader');
-
-  applyPolyfills().then(() => {
-    defineCustomElements(window);
-  });
 }
 
 export default MyApp;
